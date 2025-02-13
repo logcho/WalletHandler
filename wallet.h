9c9be321313196bbd3ef9128d1ff9a5d33e6db28
logcho <lsjcwma@gmail.com>
2025-02-13 10:59:45 -0600

    feat: implement deposit withdraw get

--- wallet.h
+++ wallet.h
@@ -12,9 +12,16 @@ class Wallet {
     public:
         Wallet();
 
-        void depositERC20(const std::string& address, uint256_t amount);
-        void withdrawERC20(const std::string& address, uint256_t amount);
-        uint256_t getERC20Balance(const std::string& address);
+        void depositERC20(const std::string& address, uint256_t amount){
+            erc20[address] += amount; // Add amount to the existing balance
+        }
+        void withdrawERC20(const std::string& address, uint256_t amount){
+            erc20[address] -= amount; // Deduct amount from balance
+        }
+        uint256_t getERC20Balance(const std::string& address){
+            auto it = erc20.find(address);
+            return (it != erc20.end()) ? it->second : uint256_t(0); // Return balance or 0 if not found 
+        }
 };
 
 #endif // WALLET_H
